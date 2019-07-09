1353b0d7fe85a2bd3bd1aaf703447243492acf8e
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-07-09 12:07:22 +0530

    Add files via upload

--- guestapp/urls.py
+++ guestapp/urls.py
@@ -4,5 +4,6 @@ urlpatterns=[
 path('',views.index,name='index'),
 path('home',views.index,name='index'),
 path('about',views.about,name='about'),
-path('contact',views.contact,name='contact')
+path('contact',views.contact,name='contact'),
+path('ser',views.services,name='services')
 ]
--- guestapp/views.py
+++ guestapp/views.py
@@ -1,50 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-	s="<html>"
-	s+="<head><style type='text/css'>*{margin:0px;} a{color:white;margin-left:20px;}</style></head>"
-	s+= "<body>"
-	s+="<header style='background-color:black;color:white;height:100px;'>"
-	s+="<h1><a href='home'>Home</a><a href='about'>About</a><a href='contact'>Contact us</a></h1>"
-	s+="</header>"
-	s+="<section style='background-color:gray;color:white;height:480px;'>"
-	s+="<p>Home page dhfjdshdhg</p>"
-	s+="</section>"
-	s+="<footer style='background-color:black;color:white;height:100px;'>"
-	s+="</footer>"
-	s+="</body>"
-	s+="</html>"
-	return HttpResponse(s)
+	return render(request,"guestapp/index.html")
 def about(request):
-	s="<html>"
-	s+="<head><style type='text/css'>*{margin:0px;} a{color:white;margin-left:20px;}</style></head>"
-	s+= "<body>"
-	s+="<header style='background-color:black;color:white;height:100px;'>"
-	s+="<h1><a href='home'>Home</a><a href='about'>About</a><a href='contact'>Contact us</a></h1>"
-	s+="</header>"
-	s+="<section style='background-color:gray;color:white;height:480px;'>"
-	s+="<p>About Page</p>"
-	s+="</section>"
-	s+="<footer style='background-color:black;color:white;height:100px;'>"
-	s+="</footer>"
-	s+="</body>"
-	s+="</html>"
-	return HttpResponse(s)
-
+	return render(request,"guestapp/about.html")
 def contact(request):
-	s="<html>"
-	s+="<head><style type='text/css'>*{margin:0px;} a{color:white;margin-left:20px;}</style></head>"
-	s+= "<body>"
-	s+="<header style='background-color:black;color:white;height:100px;'>"
-	s+="<h1><a href='home'>Home</a><a href='about'>About</a><a href='contact'>Contact us</a></h1>"
-	s+="</header>"
-	s+="<section style='background-color:gray;color:white;height:480px;'>"
-	s+="<p>Contact Page</p>"
-	s+="</section>"
-	s+="<footer style='background-color:black;color:white;height:100px;'>"
-	s+="</footer>"
-	s+="</body>"
-	s+="</html>"
-	return HttpResponse(s)   	
+    return render(request,"guestapp/contact.html") 	
 
+def services(request):
+	return render(request,"guestapp/service.html")
 
