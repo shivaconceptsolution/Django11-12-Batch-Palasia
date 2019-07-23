e77408b9e10a5033f08afab823b559887feefea8
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-07-23 12:04:53 +0530

    Add files via upload

--- stuapp/urls.py
+++ stuapp/urls.py
@@ -5,7 +5,10 @@ urlpatterns=[
 path('',views.index,name='index'),
 path('insertstudent',views.insertstudent,name='insertstudent'),
 path('viewstudent',views.viewstudent,name='viewstudent'),
+path('viewstudent1',views.viewstudent1,name='viewstudent1'),
 path('editstudent',views.editstudent,name='editstudent'),
 path('deletestudent',views.deletestudent,name='deletestudent'),
-path('updatestudent',views.updatestudent,name='updatestudent')
+path('updatestudent',views.updatestudent,name='updatestudent'),
+path('checkrno',views.checkrno,name='checkrno'),
+path('logout',views.logout,name='logout')
 ]
--- stuapp/views.py
+++ stuapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import redirect,render
 from django.http import HttpResponse
 from .models import Student
+from loginapp.models import Login
 def index(request):
-   return render(request,"stuapp/index.html")
+   if request.session.has_key('uid'):
+     s = request.session["sid"]
+     stu = Login.objects.get(pk=s)
+     return render(request,"stuapp/index.html",{'k':stu})
+   else:
+     return redirect('/loginapp') 
+def checkrno(request):
+    n = request.GET.get("q")
+    s = Student.objects.filter(rno=n)
+    if len(s)>0:
+       return HttpResponse("Rno is already exist")
+    else:
+       return HttpResponse("Valid")         
 def insertstudent(request):
   r = request.POST["txtrno"] 
   s = request.POST["txtname"]
@@ -16,6 +29,10 @@ def insertstudent(request):
 def viewstudent(request):
   s = Student.objects.all()  #select * from student
   return render(request,"stuapp/viewstudent.html",{'s':s})  
+def viewstudent1(request):
+  n = request.GET.get("q")
+  s=Student.objects.filter(name__startswith=n)
+  return render(request,"stuapp/viewstudent1.html",{'s':s})  
 
 def editstudent(request):
   sid=request.GET["q"]
@@ -37,4 +54,8 @@ def updatestudent(request):
 def deletestudent(request):
  cid=request.GET["q"]
  Student.objects.get(pk=cid).delete()
- return redirect(viewstudent)       
+ return redirect(viewstudent)   
+
+def logout(request):
+  del request.session["uid"]
+  return redirect('/loginapp')     
